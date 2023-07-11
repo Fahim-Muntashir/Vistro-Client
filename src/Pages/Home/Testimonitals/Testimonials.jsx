a62c0ce97c681e370e7e0e0568a73c53cfb3ce48
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-11 14:45:54 +0600

    review section added

--- src/Pages/Home/Testimonitals/Testimonials.jsx
+++ src/Pages/Home/Testimonitals/Testimonials.jsx
@@ -1,15 +1,17 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
 
+import "@smastrom/react-rating/style.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 const Testimonials = () => {
-  const [reviews, setReviews] = useState([]); // Corrected line
+  const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
     fetch("reviews.json")
@@ -17,43 +19,27 @@ const Testimonials = () => {
       .then((data) => setReviews(data));
   }, []);
 
-  const progressCircle = useRef(null);
-  const progressContent = useRef(null);
-
-  const onAutoplayTimeLeft = (s, time, progress) => {
-    progressCircle.current.style.setProperty("--progress", 1 - progress);
-    progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
-  };
-
   return (
     <section className="my-20">
       <SectionTitle
-        heading={"testimonials"}
-        subHeading={"what Our Client Say"}
+        subHeading="What Our Client Say"
+        heading={"Testimonials"}
       ></SectionTitle>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
-        className="mySwiper"
-      >
+
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {reviews.map((review) => (
           <SwiperSlide key={review._id}>
-            <div className="m-24">
-              <p>{review.details}</p>
+            <div className="flex flex-col items-center mx-24 my-16">
+              <Rating
+                style={{ maxWidth: 180 }}
+                value={review.rating}
+                readOnly
+              />
+              <p className="py-8">{review.details}</p>
               <h3 className="text-2xl text-orange-400">{review.name}</h3>
             </div>
           </SwiperSlide>
-        ))}{" "}
+        ))}
       </Swiper>
     </section>
   );
